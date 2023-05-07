2f972f9a94853f381071767fae1b9d99dd74aa5e
Jaidenver <jaiderhurtado271@gmail.com>
2023-05-07 10:02:43 -0500

    Crud con Spring Boot

---
+++ src/main/java/mx/com/gm/HolaSpringApplication.java
@@ -0,0 +1,40 @@
+package mx.com.gm;
+
+
+import mx.com.gm.web.ControladorInicio;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+
+
+@SpringBootApplication
+public class HolaSpringApplication implements CommandLineRunner{
+
+
+	ControladorInicio controladorInicio;
+
+	public HolaSpringApplication(ControladorInicio controladorInicio){
+		this.controladorInicio = controladorInicio;
+	}
+
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaSpringApplication.class, args);
+	}
+
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		Model model = new ExtendedModelMap();
+		controladorInicio.inicio(model);
+	}
+
+
+
+}
